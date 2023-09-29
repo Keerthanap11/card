7b1500bdfd3206ca53c6cdc8c5c32f646a871db4
Keerthanap11 <keerthanapalanivel11@gmail.com>
2023-09-29 17:10:39 +0530

    add Apiimages master

--- src/App.js
+++ src/App.js
@@ -1,68 +1,20 @@
-import{useEffect, useState} from "react";
-import "./App.css";
-import Header from "./Components/Header";
-
+import React from 'react';
+import Home from './Components/home';
+import './App.css'
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 function App() {
-  const[posts, setposts] = useState(null);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(data => data.json())
-    .then(data => {
-      setposts(data);
 
-      
-  });
-    
-  }, []);
   return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        
-          
-            {posts ? (
-              
-            
-          
-              posts.map((post) => {
-               return (
-               
-                <div className="card" >
-                <div className="card-body">
-                <h5  className="card-title">{post.id}</h5>
-                <p  className="card-text">{post.name}</p>
-                <p  className="card-text">{post.email}</p>
-                <p  className="card-text">{post.username}</p>
-                <p  className="card-text">{post.website}</p>
-                 <p  className="card-text">{post.street}</p>
-                
-                <a href ="#" className="btn btn-primary">
-                 Click Here
-                 </a>
-              </div>
-             </div>
-               
-            
-              
-                
-               );
-               })
-              
-                
-            ) : (
-              <h3>Loading......</h3>)
-            
-            
-}
-
-            
-        
-      </div>
+    <div className='bg-dark text-light'>
+      <Navbar />
+      <Home />
+      <Footer/>
     </div>
   );
-  }
+}
+
+
 
 export default App;
-
-
